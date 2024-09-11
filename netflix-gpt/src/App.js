60d4df88ebf7801166a79b1b149a3fa00fb1dbb8
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-09-11 12:48:25 +0530

    created the project, added relvant files and isntalled tailwind csS

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl" font-bold> Namaste Everyone, let's build Netflix-GPT!</h1>
+    </div>
+  );
+}
+
+export default App;
